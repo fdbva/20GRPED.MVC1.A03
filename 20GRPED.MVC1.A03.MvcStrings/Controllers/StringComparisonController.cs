7316ac3ae0b471069bc540af51305ac111927f3c
Felipe Andrade <villela.felipe@gmail.com>
2020-02-05 08:40:58 -0300

    Requisito inicial completo

--- 20GRPED.MVC1.A03.MvcStrings/Controllers/StringComparisonController.cs
+++ 20GRPED.MVC1.A03.MvcStrings/Controllers/StringComparisonController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using _20GRPED.MVC1.A03.MvcStrings.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace _20GRPED.MVC1.A03.MvcStrings.Controllers
@@ -11,6 +12,15 @@ namespace _20GRPED.MVC1.A03.MvcStrings.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Compare(StringComparisonModel stringComparisonModel)
+        {
+            var result = stringComparisonModel.Left == stringComparisonModel.Right;
+
+            stringComparisonModel.Result = result;
+            
+            return View("Result", stringComparisonModel);
         }
     }
 }
