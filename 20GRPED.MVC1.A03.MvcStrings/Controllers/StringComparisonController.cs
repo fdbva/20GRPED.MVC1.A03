98ad886dd53e34c5f38eaf641941b3f8de2db071
Felipe Andrade <villela.felipe@gmail.com>
2020-02-05 08:02:25 -0300

    Added new MVC project and Create for StringComparison

---
+++ 20GRPED.MVC1.A03.MvcStrings/Controllers/StringComparisonController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _20GRPED.MVC1.A03.MvcStrings.Controllers
+{
+    public class StringComparisonController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ 20GRPED.MVC1.A03.MvcStrings/Models/StringComparisonModel.cs
@@ -0,0 +1,9 @@
+﻿namespace _20GRPED.MVC1.A03.MvcStrings.Models
+{
+    public class StringComparisonModel
+    {
+        public string Left { get; set; }
+        public string Right { get; set; }
+        public bool Result { get; set; }
+    }
+}
